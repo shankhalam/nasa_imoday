60a779382de004110231691397f1cebc4861f747
shankhalam <shankhalamh@gmail.com>
2023-05-20 21:23:40 +0530

    add title, image and body of app

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import streamlit as st
 import requests
+from datetime import date
 
 api = "29WQxV6QbiFnP5vPH12jgG5Rm4cuIGHXafi2uEDo"
 url = "https://api.nasa.gov/planetary/apod?api_key=29WQxV6QbiFnP5vPH12jgG5Rm4cuIGHXafi2uEDo"
+today = date.today()
 
 response = requests.get(url)
+data = response.json()
 
-with open("iod.jpg", "wb") as file:
-    file.write(response.content)
+st.header(f"Image of the day from NASA on {today}")
+st.title(data["title"])
+st.image(data["url"])
+st.write(data["explanation"])
+
+st.markdown("A project by: Shan Khalam")
+
