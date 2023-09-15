b80dfd8ef52f977ab6fcef504c0abe64045e6736
Shan Khalam <dev.shankhalam@gmail.com>
2023-09-15 13:08:16 +0400

    variables name changed

--- imoday.py
+++ imoday.py
@@ -6,13 +6,13 @@ api = "29WQxV6QbiFnP5vPH12jgG5Rm4cuIGHXafi2uEDo"
 url = "https://api.nasa.gov/planetary/apod?api_key=29WQxV6QbiFnP5vPH12jgG5Rm4cuIGHXafi2uEDo"
 today = date.today()
 
-response = requests.get(url)
-data = response.json()
+response_nasa = requests.get(url)
+data_nasa = response_nasa.json()
 
 st.header(f"Image of the day from NASA on {today}")
-st.title(data["title"])
-st.image(data["url"])
-st.write(data["explanation"])
+st.title(data_nasa["title"])
+st.image(data_nasa["url"])
+st.write(data_nasa["explanation"])
 
 st.markdown("A project by: Shan Khalam")
 
